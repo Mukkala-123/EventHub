9ab66c7f6101c5e9a8e98f06fe2ac1750e4ab891
Manisha <mukkalamanishamail@gmail.com>
2026-04-08 00:14:07 +0530

    fix port

--- backend/server.js
+++ backend/server.js
@@ -467,12 +467,5 @@ app.post("/api/faculty-event-notification", (req, res) => {
 /* START SERVER                  */
 /* ============================= */
 
-
-app.listen(5000, () => console.log("✅ Server running on port 5000"));
-
-
-
-
-
-
-
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
